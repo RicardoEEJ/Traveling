3fd3f6be4c9c621627971faacf33129af7c5d0d9
Ricardo <ricardoescobar14@hotmail.com>
2022-02-24 18:21:17 +0100

    Practica 2

---
+++ app/src/main/java/com/nitropc/traveling/ActivityMainBinding.java
@@ -0,0 +1,4 @@
+package com.nitropc.traveling;
+
+public class ActivityMainBinding {
+}
---
+++ app/src/main/java/com/nitropc/traveling/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.nitropc.traveling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button buttom_next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
